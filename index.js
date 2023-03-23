6dadffe342870891ed9788667c4ac1c04a059ce2
jvloliveiraa <joao.oliveira@sptech.school>
2023-03-23 12:48:22 -0300

    Atividade feita em aula - React aula 01

---
+++ index.js
@@ -0,0 +1,30 @@
+const root = document.getElementById("root");
+
+function Participante(props) {
+
+    const [votos, setVotos] = React.useState(0);
+
+    function votar() {
+        setVotos(votos + 1);
+    }
+
+    return (
+        <>
+            <h1>{props.nome}</h1>
+            <h2>Votos: {votos}</h2>
+            <button onClick={votar}>Votar</button>
+        </>
+    );
+}
+
+function Votacao() {
+    return (
+        <>
+            <Participante nome="Resident Evil" />
+            <Participante nome="Silent Hill" />
+            <Participante nome="The dark pictures" />
+        </>
+    );
+}
+
+ReactDOM.createRoot(root).render(<Votacao />);
